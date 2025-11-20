3cbe6a0276f55ab7228865b55777062250e0609a
Monika <monikabaghumyan0@gmail.com>
2025-11-20 13:07:53 +0400

    Add images

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,30 +1,41 @@
 import React from "react";
-import '../App.css'; 
-const skillsList = [
-  { name: "HTML", img: "/html.png" },       
-  { name: "CSS", img: "/css.png" }, 
-  { name: "Sass", img: "/sass.png" },        
-  { name: "JS", img: "/javascript.png" },   
-  { name: "React", img: "/react.png" },     
-  { name: "GitHub", img: "/github.png" },    
-  { name: "Vite", img: "/vite.png" }, 
-  { name: "Git", img: "/git.png" },   
-]; 
+import '../App.css';
 
-function Skills () { 
-    return ( 
-        <div className="skills"  id="Skills"> 
-            <h1>Skills And Technologies I Use</h1> 
-            <div className="imgContainer">
-                {skillsList.map((skill, index) => (
-                    <div className="skillCard" key={index}>
-                        <img src={skill.img} alt={skill.name} />
-                        <p>{skill.name}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
-} 
+// Import images from assets
+import htmlImg from '../assets/html.png';
+import cssImg from '../assets/css.png';
+import sassImg from '../assets/sass.png';
+import jsImg from '../assets/javascript.png';
+import reactImg from '../assets/react.png';
+import githubImg from '../assets/github.png';
+import viteImg from '../assets/vite.png';
+import gitImg from '../assets/git.png';
+
+const skillsList = [
+  { name: "HTML", img: htmlImg },
+  { name: "CSS", img: cssImg },
+  { name: "Sass", img: sassImg },
+  { name: "JS", img: jsImg },
+  { name: "React", img: reactImg },
+  { name: "GitHub", img: githubImg },
+  { name: "Vite", img: viteImg },
+  { name: "Git", img: gitImg },
+];
+
+function Skills() {
+  return (
+    <div className="skills" id="Skills">
+      <h1>Skills And Technologies I Use</h1>
+      <div className="imgContainer">
+        {skillsList.map((skill, index) => (
+          <div className="skillCard" key={index}>
+            <img src={skill.img} alt={skill.name} />
+            <p>{skill.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default Skills;
